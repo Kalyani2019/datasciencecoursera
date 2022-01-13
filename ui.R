4b06a20202b5f97d304ad0c1a1cd9c5657ccc089
Kalyani2019 <47531677+Kalyani2019@users.noreply.github.com>
2022-01-13 17:56:20 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,26 +8,30 @@
 #
 
 library(shiny)
-setwd("~/DataProd-Assgnt4")
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Slide bar Data"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+           numericInput("numeric","How many Random Numbers Should be plotted?", value=1000,min=1,max=1000,step=1),
+           sliderInput("sliderX","Pick Minimum and Maximum X Values",-100,100,value=c(-50,50)),
+           sliderInput("sliderY","Pick Minimum and Maximum Y values",-100,100,value=c(-50,50)),
+           checkboxInput("show_xlab","Show/Hide X Axis Label",value=TRUE),
+           checkboxInput("show_ylab","Show/Hide Y Axis Label",value=TRUE),
+           checkboxInput("show_title","Show/Hide Title")
         ),
-
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            h3("Graph of Random Points"),
+            plotOutput("plot1")
         )
     )
 ))
+
+
+
